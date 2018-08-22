c9d0af165c809eef99baa67044779ec79bbccb16
Luca D'Amico <menteora@gmail.com>
2018-08-22 13:16:40 +0200

    feat add log example

--- windows/executor.py
+++ windows/executor.py
@@ -1,14 +1,24 @@
 import os
 import json
+import subprocess
+import logging
 
 fileDir = os.path.dirname(os.path.abspath(__file__))   # Directory of the Module
+parentDir = os.path.dirname(fileDir)
+
+logging.basicConfig(filename=parentDir + '\logs\executor.log',level=logging.DEBUG)
+logging.debug('This message should go to the log file')
+logging.info('So should this')
+logging.warning('And this, too')
+
 with open(fileDir + '\executor.config.json', 'r') as f:
     config = json.load(f)
 
-secret_key = config['DEFAULT']['SECRET_KEY'] # 'secret-key-of-myapp'
-ci_hook_url = config['CI']['HOOK_URL'] # 'web-hooking-url-from-ci-service'
-print(secret_key)
-print(ci_hook_url)
+main_task = config['MAIN_TASK']['EXEC'] 
+print(main_task)
+return_code = subprocess.run(main_task).returncode
+print(return_code)
+
 
 input("Press enter to continue...")
 
