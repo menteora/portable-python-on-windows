07b901d806fa3e45fb16113ce74f8bc4a93910e2
Luca D'Amico <menteora@gmail.com>
2018-08-22 14:25:22 +0200

    feat: created logger library

--- scripts/executor.py
+++ scripts/executor.py
@@ -1,15 +1,20 @@
 import os
 import json
 import subprocess
-import logging
+import importlib.util
 
-fileDir = os.path.dirname(os.path.abspath(__file__))   # Directory of the Module
+fileDir = os.path.dirname(os.path.abspath(__file__))
 parentDir = os.path.dirname(fileDir)
 
-logging.basicConfig(filename=parentDir + '\logs\executor.log',level=logging.DEBUG)
-logging.debug('This message should go to the log file')
-logging.info('So should this')
-logging.warning('And this, too')
+spec = importlib.util.spec_from_file_location("logger", fileDir + "/logger.py")
+logger = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(logger)
+
+# logging.basicConfig(filename=parentDir + '\logs\executor.log',level=logging.DEBUG)
+log = logger.setup('myapp', parentDir)
+log.debug('This message should go to the log file')
+log.info('So should this')
+log.warning('And this, too')
 
 with open(fileDir + '\executor.config.json', 'r') as f:
     config = json.load(f)
