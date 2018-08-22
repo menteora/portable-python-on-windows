2728fd22af259ac4377b3aa2e9d3415314a7102d
Luca D'Amico <menteora@gmail.com>
2018-08-22 17:28:47 +0200

    feat: email work

--- scripts/mailer.py
+++ scripts/mailer.py
@@ -1,6 +1,10 @@
 import smtplib
 import json
 import os
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 
 def send_email_gmail():
 
@@ -8,37 +12,48 @@ def send_email_gmail():
     parentDir = os.path.dirname(fileDir) 
     logPath = os.path.join(parentDir, 'logs') 
     log_files = os.listdir(logPath)
-    print(log_files)
 
     for index, log_file in enumerate(log_files):
         log_files[index] = logPath + "\\" + log_file
-    print(log_files)
-    
+
+
     with open(fileDir + '\mailer.config.json', 'r') as f:
         config = json.load(f)
 
     gmail_user = config['authentication']['user']
     gmail_password =  config['authentication']['password']
 
-    sent_from = gmail_user  
-    to = config['authentication']['to']
-    subject = 'OMG Super Important Message'  
-    body = 'Hey, whats up?\n\n- You'
+    fromaddr = gmail_user
+    toaddr = ', '.join(config['authentication']['to']) 
+    print(toaddr)
+    msg = MIMEMultipart()
+    
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = "SUBJECT OF THE EMAIL"
+ 
+    body = "TEXT YOU WANT TO SEND"
+ 
+    msg.attach(MIMEText(body, 'plain'))
+ 
+    filename = "executor.log"
+    attachment = open("logs\\" + filename, "rb")
+ 
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload((attachment).read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+ 
+    msg.attach(part)
 
-    email_text = """\  
-    From: %s  
-    To: %s  
-    Subject: %s
-
-    %s
-    """ % (sent_from, ", ".join(to), subject, body)
 
     try:  
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.ehlo()
         server.starttls()
         server.login(gmail_user, gmail_password)
-        server.sendmail(sent_from, to, email_text, log_files)
+        text = msg.as_string()
+        server.sendmail(fromaddr, config['authentication']['to'], text)
         server.close()
 
         print("Email sent")
