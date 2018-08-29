802f571eaa418f323221b4cdac9bb90448759789
Luca D'Amico <menteora@gmail.com>
2018-08-29 17:17:20 +0200

    feat: add mssql first release

--- scripts/mssql_extractor.py
+++ scripts/mssql_extractor.py
@@ -1,7 +1,28 @@
 from sqlalchemy import create_engine
-conn_str = r'mssql+pymssql://(local)\SQLEXPRESS/myDb'
+from pandas import DataFrame
+import pandas as pd
+
+#https://www.dataquest.io/blog/excel-and-pandas/
+fileDir = os.path.dirname(os.path.abspath(__file__))
+
+with open(fileDir + '\mssql_extractor.config.json', 'r') as f:
+    config = json.load(f)
+
+user = config['authentication']['user']
+password = config['authentication']['password']
+host = config['authentication']['host']
+database = config['authentication']['database']
+query = config['authentication']['query']
+
+conn_str = 'mssql+pymssql://'+user+':'+password+'@'+host+'/'+database
 engine = create_engine(conn_str)
 connection = engine.connect()
-result = connection.execute("SELECT SYSTEM_USER AS me")
-row = result.fetchone()
-print(row['me'])
+result = connection.execute(query)
+
+df = DataFrame(result.fetchall())
+df.columns = result.keys()
+
+writer = pd.ExcelWriter('output.xlsx', engine='xlsxwriter')
+
+df.to_excel(writer, index=False, sheet_name='report')
+writer.save()
